0c7ec06bf41d6653b512c83d6aa27abc1299832d
Sarah Alhajli <sarahalh@live.unc.edu>
2022-11-21 17:24:12 -0500

    Fixing help message

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ import fetch from 'node-fetch';
 const args = minimist(process.argv.slice(2));
 const timezone = moment.tz.guess();
 
-if(args.includes('-h')) {
+if(args.h) {
 	console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE'
     + '-h            Show this help message and exit.'
     + '-n, -s        Latitude: N positive; S negative.'
@@ -16,7 +16,7 @@ if(args.includes('-h')) {
     + '-j            Echo pretty JSON from open-meteo API and exit.')
 }
 
-if(args.includes('-z')) {
+if(args.z) {
 	timezone = args.z
 }
 
@@ -30,6 +30,6 @@ if (days == 0) {
   console.log("tomorrow.")
 }
 
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + 'daily=precipitation_hours');
+//const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + 'daily=precipitation_hours');
 
 
