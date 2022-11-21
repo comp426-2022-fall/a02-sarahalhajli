012637e19c1fcb847dfcb126d098a58bd070bc2e
Sarah Alhajli <sarahalh@live.unc.edu>
2022-11-21 17:17:55 -0500

    Editing cli.js

--- cli.js
+++ cli.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-//import minimist;
+import minimist from 'minimist';
 import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 
 const [,, ...args] = process.argv
+const args = require('minimist')(process.argv.slice(2));
+const timezone = moment.tz.guess();
 
 if(args.includes('-h')) {
 	console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE'
@@ -15,6 +17,20 @@ if(args.includes('-h')) {
     + '-j            Echo pretty JSON from open-meteo API and exit.')
 }
 
+if(args.includes('-z')) {
+	timezone = args.z
+}
 
-const timezone = moment.tz.guess();
+const days = args.d 
+
+if (days == 0) {
+  console.log("today.")
+} else if (days > 1) {
+  console.log("in " + days + " days.")
+} else {
+  console.log("tomorrow.")
+}
+
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + 'daily=precipitation_hours');
+
 
