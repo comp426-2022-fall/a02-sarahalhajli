b2df7d9787be2146bb1d6548d1e67073b61a3bca
Sarah Alhajli <sarahalh@live.unc.edu>
2022-11-22 02:14:32 -0500

    edited cli.js

--- cli.js
+++ cli.js
@@ -4,20 +4,20 @@ import moment from 'moment-timezone';
 import fetch from 'node-fetch';
 
 const args = minimist(process.argv.slice(2));
-const timezone = moment.tz.guess();
+var timezone = moment.tz.guess();
 
 if(args.h) {
-	console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE'
-    + '-h            Show this help message and exit.'
-    + '-n, -s        Latitude: N positive; S negative.'
-    + '-e, -w        Longitude: E positive; W negative.'
-    + '-z            Time zone: uses tz.guess() from moment-timezone by default.'
-    + '-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.'
-    + '-j            Echo pretty JSON from open-meteo API and exit.')
+	console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n'
+    + '-h            Show this help message and exit.\n'
+    + '-n, -s        Latitude: N positive; S negative.\n'
+    + '-e, -w        Longitude: E positive; W negative.\n'
+    + '-z            Time zone: uses tz.guess() from moment-timezone by default.\n'
+    + '-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.\n'
+    + '-j            Echo pretty JSON from open-meteo API and exit.\n')
 }
 
 if(args.z) {
-	timezone = args.z
+	timezone = args.z;
 }
 
 const days = args.d 
@@ -30,6 +30,25 @@ if (days == 0) {
   console.log("tomorrow.")
 }
 
-//const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + 'daily=precipitation_hours');
+var latitude = 0;
+if(args.n) {
+	latitude = args.n;
+	if(args.s){
+		latitude = args.s;
+	}
+}
 
+var longitude = 0;
+if(args.e) {
+	longitude = args.e;
+	if(args.w){
+		longitude = args.w;
+	}
+}
 
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + 'daily=precipitation_hours');
+const data = await response.json();
+
+if(args.j) {
+	console.log(data);
+}
